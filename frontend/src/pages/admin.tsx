2424328779c9e30000bf1963bc8b85f7ef4ed203
Ben Dechrai <ben@dechrai.com>
2020-09-14 22:53:52 +1000

    Added RBAC

--- frontend/src/pages/admin.tsx
+++ frontend/src/pages/admin.tsx
@@ -50,10 +50,16 @@ const AdminPage: React.FC<PageProps<DataProps>> = ({ data }) => {
         },
         method: editItem?.id ? "PUT" : "POST",
         body: JSON.stringify({ item: editItem }),
-      }).then(() => {
-        setEditItem(null)
-        loadItems()
       })
+        .then(res => {
+          if (res.status !== 200) {
+            alert("You don't have permission to do this.")
+            return
+          }
+          setEditItem(null)
+          loadItems()
+        })
+        .catch(() => alert("You don't have permission to do this."))
     })
   }
 
@@ -64,7 +70,11 @@ const AdminPage: React.FC<PageProps<DataProps>> = ({ data }) => {
           Authorization: `Bearer ${token}`,
         },
         method: "DELETE",
-      }).then(() => {
+      }).then(res => {
+        if (res.status !== 200) {
+          alert("You don't have permission to do this.")
+          return
+        }
         loadItems()
       })
     })
